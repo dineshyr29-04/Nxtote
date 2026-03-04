81cb513ef3fe518085825c2517c96d9f670c4ce6
dineshyr29-04 <dineshyr2904@gmail.com>
2026-03-04 12:27:37 +0530

    has some practice

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Intro} from './intro.jsx';
+
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const founders=["Dinesh","Vicky","Anand","Dhanush Shenoy"]
   return (
     <>
+      
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Intro />
+        <h1 className="h">Founders of Cardio Nerve</h1>
+        <ul>
+          {founders.map(item =>{
+             return <li className="founders">{item}</li>
+          })}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/intro.jsx
@@ -0,0 +1,13 @@
+
+
+
+
+export function Intro(){
+    return (
+    <>
+        <h1>Dinesh <span>A</span></h1>
+    </>
+)
+};
+export const string="lawl";
+
