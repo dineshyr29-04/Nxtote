ab926c7d85bac038c67fa169a0f1de400e302d73
dineshyr29-04 <dineshyr2904@gmail.com>
2026-03-05 21:01:52 +0530

    some practice done one card and card scanner created

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {Intro} from './intro.jsx';
 
+function List({founders,className}){
+  return <ul className={className}>{founders.map(founder=>{
+    return  <li key={founder.name} className="founder-item">{founder.role} - {founder.name}</li>
+  })}</ul>
+}     
 
 function App() {
   const [count, setCount] = useState(0)
-  const founders=["Dinesh","Vicky","Anand","Dhanush Shenoy"]
+  
+  const founders= [
+    {role: "Software Developer",name:"Dinesh A"},
+    {role:"IoT Developer", name:"Anand M"},
+    {role:"ML Engineer", name:"Dhanush Shenoy H"},
+    {role:"Product Designer", name:"Vignesh A"}
+  ]
   return (
     <>
-      
-      <div>
-        <Intro />
-        <h1 className="h">Founders of Cardio Nerve</h1>
-        <ul>
-          {founders.map(item =>{
-             return <li className="founders">{item}</li>
-          })}
-        </ul>
+      <div className="box">
+        <div className="scan"></div>
+        <h1>Founders</h1>
+        <List founders={founders} className="founders"  />
       </div>
     </>
   )
