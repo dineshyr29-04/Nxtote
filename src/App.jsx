3e730458b3e082f60525135b8041d22a090fc89c
dineshyr29-04 <dineshyr2904@gmail.com>
2026-03-10 23:03:32 +0530

    some things are changed and background things changed

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,31 @@ import "./index.css"
 import DarkModeToggler from './components/darktoggler';
 import Basic from './components/basic';
 import { FlipButton } from './components/buttonreverser';
-function App(){
+const showContext=["Full-stack Developer with a passion for crafting seamless user experiences and robust backend solutions. Skilled in JavaScript, React.js, Node.js, Python, C++, and Express.js. Dedicated to delivering high-quality code and innovative solutions."]
+const textSpeller=showContext[0];
+;function App(){
   
     
   
   return (
     <div className="background-1">
-      <div className='heading'>
-        <h1>DINESH</h1>
-        <br/> 
-        <h1>A</h1>
-        <div>
-
+      <div className="background"></div>
+      <div className="headcontainer">
+        <div className='heading'>
+          <h1>DINESH</h1>
+           
+          <h1>A</h1>
+        </div>
+        
+        <div className="content">
+          <textSpeller />
         </div>
       </div>
       
       <DarkModeToggler />
       <div className="container">
         
-        {/*<div className='pagechanger'>
-            <FlipButton  text1="Go back to next section" text2="Change to Next section"/>
-        </div>*/}
+        
         
       </div>
     </div>
