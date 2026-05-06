cd2459f3e5024ef9cb2670007af0b36a17f13ea5
dineshyr29-04 <dineshyr2904@gmail.com>
2026-05-06 11:22:23 +0530

    the prittier changed the code a little bit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import {useState,uyseEffect} from 'react';
-import './App.css';
-import {BrouserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+import { useState, uyseEffect } from "react";
+import "./App.css";
+import { BrouserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
-
-function App(){
-  return(
-    <> 
-    <h1>Welcome to my React App</h1> 
+function App() {
+  return (
+    <>
+      <h1>Welcome to my React App</h1>
     </>
-  )
+  );
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,31 @@
-import {useState,useEffect} from 'react';
-import './Navbar.css';
-import {Link} from 'react-router-dom';  
+import { useState, useEffect } from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
 
-const navbarlinks=[
-    {name:"Home",path:"/home"},
-    {name:"About",path:"/about"},
-    {name:"Contact",path:"/contact"}
-]
-function Navbar(){
-    return  (
-        <>
-            <div className="navbar-container">
-                 <div classname="navbar-logo">
-                    <link to="/">
-                        <img src="" alt="logo" classname="navbar-logo" />
-                    </link>
-                 </div>
-                 <div classname="navbar-menu">
-                    <ul classname="navbar-links">
-                        {navbarlinks.map((link,index)=>(
-                            <li key={index} classname="navbar-link">
-                                <Link to={link.path}>{link.name}</Link>
-                            </li>
-                        ))}                     
-                    </ul>
-
-                 </div>
-            </div>
-        </>
-    )
+const navbarlinks = [
+  { name: "Home", path: "/home" },
+  { name: "About", path: "/about" },
+  { name: "Contact", path: "/contact" },
+];
+function Navbar() {
+  return (
+    <>
+      <div className="navbar-container">
+        <div classname="navbar-logo">
+          <link to="/">
+            <img src="" alt="logo" classname="navbar-logo" />
+          </link>
+        </div>
+        <div classname="navbar-menu">
+          <ul classname="navbar-links">
+            {navbarlinks.map((link, index) => (
+              <li key={index} classname="navbar-link">
+                <Link to={link.path}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
