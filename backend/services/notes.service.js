c80f944cc60fb35cb73b434edca48df135d0d696
dineshyr29-04 <dineshyr2904@gmail.com>
2026-08-03 18:40:55 +0530

    feat:added service for update note

--- backend/services/notes.service.js
+++ backend/services/notes.service.js
@@ -11,17 +11,30 @@ exports.getAllNotes = async (req, res) => {
   return data;
 };
 
-exports.createNote=async (notedata)=>{
-  const {data , error} =await supabase
-        .from("notes")
-        .insert(
-          notedata
-      )
-        .select()
-        .single();
+exports.createNote = async (notedata) => {
+  const { data, error } = await supabase
+    .from("notes")
+    .insert(notedata)
+    .select()
+    .single();
 
-        if (error) {
-          throw error;
-        }
-        return data;
-}
+  if (error) {
+    throw error;
+  }
+  return data;
+};
+
+exports.updateNote = async (updates) => {
+  const { data, error } = await supabase
+    .from("notes")
+    .update(updates)
+    .eq("id", id)
+    .select()
+    .single();
+
+  if (error) {
+    throw error
+  }
+
+  return data;
+};
