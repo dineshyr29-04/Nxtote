e471cc7212a772fd23696260fdb45e15c228ad4f
dineshyr29-04 <dineshyr2904@gmail.com>
2026-08-06 09:17:54 +0530

    feat(app.js)rearranged the order of code to express

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,21 @@
-const express =require('express');
-const notesRouter = require('./routes/notesroutes.js');
-const authrouter = require('./routes/authroute.js');
-const {authenticateuser} = require('./middleware/authmiddleware');
-const { errorhandler } = require('./middleware/errormiddleware.js');
-const cors =require('cors');
+const express = require("express");
+const notesRouter = require("./routes/notesroutes.js");
+const authrouter = require("./routes/authroute.js");
+const { authenticateuser } = require("./middleware/authmiddleware");
+const { errorhandler } = require("./middleware/errormiddleware.js");
+const cors = require("cors");
 
-
-const app=express();
-
+const app = express();
+app.use(
+  cors({
+    oringin: ["http://localhost:5173", "https://nxtote.vercel.app/"],
+  }),
+);
 app.use(express.json());
-app.use(cors());
 
-app.use("/",authenticateuser,notesRouter);
+
+app.use("/", authenticateuser, notesRouter);
 app.use("/auth", authrouter);
-
 
 app.use(errorhandler);
 
