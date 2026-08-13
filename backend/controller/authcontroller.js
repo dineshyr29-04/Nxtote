aa236c2f5268038e656c25ca5a66ebd15e82c36b
dineshyr29-04 <dineshyr2904@gmail.com>
2026-08-13 10:10:58 +0530

    fdeat(backend)added the apierror and reponse in the authcontroller

--- backend/controller/authcontroller.js
+++ backend/controller/authcontroller.js
@@ -1,29 +1,28 @@
 const authservice = require("../services/authservices.js");
 const asynchandler = require("../utils/asynchandler.js");
+const Apierror = require("../utils/apierror.js");
+const apiResponse = require("../utils/apiresponse.js");
+
 exports.register = asynchandler(async (req, res) => {
     const { name, email, password } = req.body;
     console.log({ name, email, password });
     if (!email || !password) {
         console.log("Email or password any one is wrong");
-        return res.status(400).json({
-            message: "User Credentials are Required",
-        });
+        throw new Apierror(400, "User credentials Required");
     }
     const user = await authservice.register(name, email, password);
     console.log("Registered");
-    res.status(201).json(user);
+    res.status(201).json(new apiResponse(true, "Registered Successfuly", user));
 });
 
 exports.login = asynchandler(async (req, res) => {
     const { email, password } = req.body;
     if (!email || !password) {
         console.log("error in passwords");
-        return res.status(400).json({
-            message: "Required email and password.",
-        });
+        throw new Apierror(400, "User credentials Required");
     }
 
     const data = await authservice.login({ email, password });
     console.log("loggedin");
-    res.status(200).json(data);
+    res.status(200).json(new apiResponse(true, "Loggedi Successfuly", data));
 });
